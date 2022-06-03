112b79d86a235a34539169c74c2efa00f5cde2ec
huycan <vohuy21032001@gmail.com>
2022-06-04 00:25:00 +0700

    feature edit todo and edit check all

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 
 // components
 import Input from './components/Input';
-// import TrafficLight from './components/TrafficLight';
+import TrafficLight from './components/TrafficLight';
 
 // Font Awesomeicon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -26,23 +26,28 @@ class App extends Component {
             newItem: '',
             todoItems: [
                 {
+                    id: 1,
                     title: 'go to work',
                     isComplete: true,
                 },
                 {
+                    id: 2,
                     title: 'go to school',
                     isComplete: false,
                 },
                 {
+                    id: 3,
                     title: 'go to bed',
                     isComplete: false,
                 },
             ],
+            editTodo: {},
         };
 
         // this.onKeyUp = this.onKeyUp.bind(this);
     }
 
+    // handle when todo complete
     handleTodoComplete = (item) => {
         const { todoItems } = this.state;
         const isComplete = item.isComplete;
@@ -54,6 +59,7 @@ class App extends Component {
         });
     };
 
+    // handle add todo
     handleAddTodo = (event) => {
         // Enter key
         if (event.keyCode === 13) {
@@ -68,31 +74,36 @@ class App extends Component {
                 newItem: '',
                 todoItems: [
                     ...this.state.todoItems,
-                    { title: text, isComplete: false },
+                    {
+                        id: Math.floor(Math.random() * 100000),
+                        title: text,
+                        isComplete: false,
+                    },
                 ],
             });
             toast.success('Success!');
         }
     };
 
+    // handle on change
     handleChangeInput = (event) => {
         this.setState({
             newItem: event.target.value,
         });
     };
 
+    //  handle check all
     handleCheckAll = () => {
         const { todoItems } = this.state;
 
         this.setState({
             todoItems: todoItems.map((i) =>
-                i.isComplete
-                    ? { ...i, isComplete: !i.isComplete }
-                    : { ...i, isComplete: !i.isComplete },
+                !i.isComplete ? { ...i, isComplete: true } : { ...i },
             ),
         });
     };
 
+    // handle delete
     handleDeleteTodo = (item) => {
         const { todoItems } = this.state;
         const index = todoItems.indexOf(item);
@@ -106,8 +117,75 @@ class App extends Component {
         toast.success('Delete successfully!!');
     };
 
+    // Edit todo
+    // DB Click todo
+    handleDBClickEditTodo = (item) => {
+        const { todoItems } = this.state;
+
+        this.setState({
+            editTodo: item,
+            todoItems: todoItems.map((i) =>
+                i === item && i.isComplete
+                    ? { ...i, isComplete: !i.isComplete }
+                    : { ...i },
+            ),
+        });
+    };
+
+    // handle on change
+    handleOnChangeEditTodo = (event) => {
+        let editTodoCopy = this.state.editTodo;
+        editTodoCopy.title = event.target.value;
+
+        this.setState({
+            editTodo: editTodoCopy,
+        });
+    };
+
+    // handle update
+    handleUpdateEditTodo = (event, item) => {
+        const { todoItems, editTodo } = this.state;
+        const isEmptyObj = Object.keys(editTodo).length === 0;
+        // Enter key
+        if (event.keyCode === 13) {
+            let text = event.target.value;
+            text = text.trim();
+            if (!text) {
+                toast.error('Missing require!');
+                return;
+            }
+            if (isEmptyObj === false && editTodo.id === item.id) {
+                let todoItemsCopy = [...todoItems];
+
+                console.log(todoItemsCopy);
+
+                const objIndex = todoItemsCopy.findIndex(
+                    (i) => i.id === item.id,
+                );
+
+                todoItemsCopy[objIndex].title = editTodo.title;
+
+                this.setState({
+                    todoItems: todoItemsCopy,
+                    editTodo: {},
+                });
+                toast.success('Update successful!');
+                return;
+            }
+        }
+    };
+
+    // handle when blur input edit
+    handleOnBlurInput = () => {
+        this.setState({
+            editTodo: {},
+        });
+    };
+
     render() {
-        const { todoItems, newItem } = this.state;
+        const { todoItems, newItem, editTodo } = this.state;
+
+        let isEmptyObj = Object.keys(editTodo).length === 0;
 
         return (
             <div className="App">
@@ -128,7 +206,7 @@ class App extends Component {
 
                     {todoItems.length > 0 &&
                         todoItems.map((item, index) => (
-                            <div className="wrap" key={index}>
+                            <div className="wrap" key={item.id}>
                                 <div
                                     onClick={() =>
                                         this.handleTodoComplete(item)
@@ -163,7 +241,48 @@ class App extends Component {
                                         itemComplete: item.isComplete,
                                     })}
                                 >
-                                    <p>{item.title}</p>
+                                    {isEmptyObj === true ? (
+                                        <p
+                                            onDoubleClick={() =>
+                                                this.handleDBClickEditTodo(item)
+                                            }
+                                        >
+                                            {item.title}
+                                        </p>
+                                    ) : (
+                                        <>
+                                            {editTodo.id === item.id ? (
+                                                <input
+                                                    className="input-edit"
+                                                    onChange={(event) =>
+                                                        this.handleOnChangeEditTodo(
+                                                            event,
+                                                        )
+                                                    }
+                                                    onBlur={() =>
+                                                        this.handleOnBlurInput()
+                                                    }
+                                                    onKeyUp={(event) =>
+                                                        this.handleUpdateEditTodo(
+                                                            event,
+                                                            item,
+                                                        )
+                                                    }
+                                                    value={editTodo.title}
+                                                ></input>
+                                            ) : (
+                                                <p
+                                                    onDoubleClick={() =>
+                                                        this.handleDBClickEditTodo(
+                                                            item,
+                                                        )
+                                                    }
+                                                >
+                                                    {item.title}
+                                                </p>
+                                            )}
+                                        </>
+                                    )}
 
                                     <div className="icon icon-del">
                                         <FontAwesomeIcon
@@ -182,7 +301,7 @@ class App extends Component {
                     )}
                 </div>
 
-                {/* <TrafficLight /> */}
+                <TrafficLight />
 
                 <ToastContainer
                     position="top-right"
